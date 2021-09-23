d2218e7b174dd95cff33093811e3f89a7a3a2e02
HaVanPhong <haphonggoroki@gmail.com>
2021-09-23 07:15:19 +0700

    demo chat with a room

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 const username= document.getElementById('username').innerHTML.substr(6);
 const room= document.getElementById('room').innerHTML.substr(6);
-var socket = io.connect('https://chatting-gr.herokuapp.com');
+
+var socket = io.connect(process.env.DOMAIN);
 socket.on('connect', function (data) {
     socket.emit('join', room);
 });
